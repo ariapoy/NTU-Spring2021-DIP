a635264cfaa8068bfd5ee996c65d67026aef890e
ariapoy <ariapoy@gmail.com>
2021-05-26 01:02:37 +0800

    Finish 2 (b) & (c)?

--- hw4/src/prob-2-frequency-domain.py
+++ hw4/src/prob-2-frequency-domain.py
@@ -49,13 +49,80 @@ print("Finish prob 2 (a).")
 Based on the result of part (a), design and apply a low-pass filter in the frequency domain and transform the result back to the pixel domain by inverse Fourier transform. The resultant image is saved as \textbf{result6.png}. Please also design a low-pass filter in the pixel domain which behaves similarly to the one you design in the frequency domain. Output the result as \textbf{result7.png} and provide some discussions.
 """
 
-def filter_on_freqdomain(img_arr, filter_name="Gaussian_low-pass"):
+def filter_on_freqdomain(img_arr, filter_name="Gaussian_low-pass", D0=10, D1=1000):
+    """
+    D0: cutoff freq of Gaussian filter (\sigma)
+    """
     F = shift_fft(img_arr)
+    M, N = img_arr.shape
     if filter_name == "Gaussian_low-pass":
-        D0 = 10
-        H = None
-    pdb.set_trace()
-    return img_arr
+        D = np.fromfunction(lambda u, v: np.sqrt( (u - M//2)**2 + (v - N//2)**2), (M, N))
+        H = np.exp(-D**2/(2*D0**2))
+    elif filter_name == "Gaussian_high-pass":
+        D = np.fromfunction(lambda u, v: np.sqrt( (u - M//2)**2 + (v - N//2)**2), (M, N))
+        H = 1 - np.exp(-D**2/(2*D0**2))
+        H = np.exp(-D**2/(2*D1**2)) - np.exp(-D**2/(2*D0**2))
+    elif filter_name == "Laplacian_high-pass":
+        D = np.fromfunction(lambda u, v: np.sqrt( (u - M//2)**2 + (v - N//2)**2), (M, N))
+        H = -(D)
+    G = H*F
+    return G
 
-result6_arr = filter_on_freqdomain(sample2_arr)
+def ifft_shift(img_arr):
+    def shift_arr(img_arr):
+        shift_scaler = np.fromfunction(lambda i, j: (-1)**(i + j), img_arr.shape)
+        res_arr = img_arr*shift_scaler
+        return res_arr
+    img_center = ifft2(img_arr)
+    g = shift_arr(img_center)
+    res_arr = utils.int_round(g)
+    return res_arr
+
+def filter_on_spatdomain(img_arr, filter_name="Gaussian_low-pass", D0=10, D1=1000, k=5):
+    """
+    D0: cutoff freq of Gaussian filter (\sigma)
+    k: kernel size of filter
+    """
+    M, N = img_arr.shape
+    if filter_name == "Gaussian_low-pass":
+        h = np.fromfunction(lambda u, v: 1/(2*np.pi*D0**2)*np.exp(-((u - k//2)**2 + (v - k//2)**2)/(2*D0**2)), (k, k) )
+        #h = np.fromfunction(lambda u, v: np.exp(-((u - k//2)**2 + (v - k//2)**2)/(2*D0**2)), (k, k) )
+        h = h / np.sum(h)
+    elif filter_name == "Gaussian_high-pass":
+        h2 = np.fromfunction(lambda u, v: 1/(2*np.pi*D0**2)*np.exp(-((u - k//2)**2 + (v - k//2)**2)/(2*D0**2)), (k, k) )
+        h1 = np.fromfunction(lambda u, v: 1/(2*np.pi*D1**2)*np.exp(-((u - k//2)**2 + (v - k//2)**2)/(2*D1**2)), (k, k) )
+        h = h2 - h1
+        #h = np.fromfunction(lambda u, v: 1/(2*np.pi*D0**2)*np.exp(-((u - k//2)**2 + (v - k//2)**2)/(2*D0**2)), (k, k) )
+        h = -1/h[0, 0]*h
+        h[k//2, k//2] = -(h.sum() - h[k//2,k//2]) + h[k//2,k//2]
+    elif filter_name == "Laplacian_high-pass":
+        h = np.array([
+                      [0,  1, 0], 
+                      [1, -4, 1],
+                      [0,  1, 0]
+                     ])
+
+    res_arr = convolve2d(img_arr, h, boundary="symm", mode="same")
+    return res_arr
+
+result6_arr = ifft_shift(filter_on_freqdomain(sample2_arr, D0=50) )
+utils.save_npArr2JPG(result6_arr, "result6")
+result7_arr = filter_on_spatdomain(sample2_arr, D0=50, k=5)
+utils.save_npArr2JPG(result7_arr, "result7")
+print("Finish prob 2 (b).")
+
+# prob 2(c)
+"""
+Based on the result of part (a), design and apply a high-pass filter in the frequency domain and transform the result back to the pixel domain by inverse Fourier transform. The resultant image is saved as \textbf{result8.png}. Please also design a high-pass filter in the pixel domain which behaves similarly to the one you design in the frequency domain. Output the result as \textbf{result9.png} and provide some discussions.
+"""
+
+result8_G_arr = ifft_shift(filter_on_freqdomain(sample2_arr, filter_name="Gaussian_high-pass", D0=50) )
+utils.save_npArr2JPG(result8_G_arr, "tmp/result8-Gaussian")
+result9_G_arr = filter_on_spatdomain(sample2_arr, filter_name="Gaussian_high-pass", D0=50, k=5)
+utils.save_npArr2JPG(result9_G_arr, "tmp/result9-Gaussian")
+result8_arr = ifft_shift(filter_on_freqdomain(sample2_arr, filter_name="Laplacian_high-pass", D0=50) )
+utils.save_npArr2JPG(result8_arr, "result8")
+result9_arr = filter_on_spatdomain(sample2_arr, filter_name="Laplacian_high-pass", D0=50, k=5)
+utils.save_npArr2JPG(result9_G_arr, "result9")
+print("Finish prob 2 (c).")
 
